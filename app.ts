4cc057442fb7bc17155377237b251ccd3859f8ba
Veren Villegas <verenvillegas@gmail.com>
2024-02-29 21:10:51 +1300

    Updated /interactions code

--- app.ts
+++ app.ts
@@ -5,7 +5,9 @@ import {
   InteractionResponseFlags,
   MessageComponentTypes,
   ButtonStyleTypes,
+  verifyKeyMiddleware,
 } from "discord-interactions";
+import { Client } from "discord.js";
 
 const app = express();
 
@@ -16,29 +18,47 @@ app.use(express.json());
 /**
  * Interactions endpoint. This is where all of our requests from Discord will be sent to.
  */
-app.post("/interactions", (req: Request, res: Response) => {
-  //Check if the request is valid.
-  //Check if in VC
-  //IF not in VC AND request = play then
-  //Join VC
-  //Else
-  //Send error saying need to join vc first.
-  //Play song request
-  //Play song
-  //Pause song request
-  //Check if in vc
-  //Pause song
-  //Skip song request
-  //Check if in vc
-  //Skip song
-  //Queue song request
-  //Check if in vc
-  //Add song to queue
-  //Disconnect request
-  //Check if connected
-  //Disconnect
-  //Handle error
-});
+app.post(
+  "/interactions",
+  verifyKeyMiddleware(process.env.PUBLIC_KEY || ""),
+  (req: express.Request, res: express.Response) => {
+    //Extract the type, ID and data of the request.
+    const message = req.body;
+
+    //Check what type the request is.
+    if (message !== null) {
+      if (message.type === InteractionType.APPLICATION_COMMAND) {
+        const name = message.data;
+        if (name === "play") {
+          return res.send({
+            type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+            data: { content: "Voila!" },
+          });
+        }
+      }
+    }
+    //Check if in VC
+    //IF not in VC AND request = play then
+    //Join VC
+    //Else
+    //Send error saying need to join vc first.
+    //Play song request
+    //Play song
+    //Pause song request
+    //Check if in vc
+    //Pause song
+    //Skip song request
+    //Check if in vc
+    //Skip song
+    //Queue song request
+    //Check if in vc
+    //Add song to queue
+    //Disconnect request
+    //Check if connected
+    //Disconnect
+    //Handle error
+  }
+);
 
 app.listen(PORT, () => {
   console.log(`Listening on ${PORT}`);
